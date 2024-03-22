46f145fc73c285eb9d1d52be310a484d49fed61d
박서영 <seokachuu@gmail.com>
2024-03-22 21:46:37 +0900

    Feat: nav 공사중 추가

--- app/medical/page.tsx
+++ app/medical/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const page = () => {
-  return <div></div>;
+const Medical = () => {
+  return (
+    <section className="h-screen flex items-center justify-center">
+      <div className="text-5xl">공사중입니다!</div>
+    </section>
+  );
 };
 
-export default page;
+export default Medical;

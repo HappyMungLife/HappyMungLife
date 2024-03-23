c551880a19bc29fcac9510bd304f4cf82df5388e
박서영 <seokachuu@gmail.com>
2024-03-23 17:29:31 +0900

    Feat: api 설정 추가

---
+++ app/_utils/supabase/api.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Supabase 클라이언트 초기화
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

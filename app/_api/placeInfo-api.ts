d00cc8e9a1acaa716dd272f5ff8bf72e22e40035
Hyeryeon-Seo <0168968060@naver.com>
2024-03-24 19:09:58 +0900

    Feat: 병원/약국 정보 가져오기

--- app/_api/placeInfo-api.ts
+++ app/_api/placeInfo-api.ts
@@ -1,15 +1,21 @@
 import axios from 'axios';
 
+// .env.local에 넣기
 const URL =
   'https://api.odcloud.kr/api/15111389/v1/uddi:41944402-8249-4e45-9e9d-a52d0a7db1cc?page=1&perPage=10&serviceKey=9L5HwXwc21vzjZElotvS2os724s6JtXyr%2FVsac%2BpQrJ1TV6Vc4IVvN9HxQcWAQH3qMLi9GvMQOLLtzrftdJjcQ%3D%3D';
 
-export const fetchAniPharms = async () => {
-  // 페이지네이션 어케하지 ㅠ 데이터..일단 다 가져오나보자..
+export const fetchPharmsList = async () => {
   const { data } = await axios.get(URL);
   const placeInfos: [] = data.data;
-  const aniPharms = placeInfos.filter((placeInfo: any) => placeInfo['카테고리3'] == '동물약국'); // map 처럼 안에 () => {} 중괄호 쓰면안됨 .
+  const pharmsList = placeInfos.filter((placeInfo: any) => placeInfo['카테고리3'] == '동물약국'); // map 처럼 안에 () => {} 중괄호 쓰면안됨 .
+  return pharmsList;
+};
 
-  //   console.log(placeInfos[0]);
-  //   console.log(da);
-  return aniPharms;
+export const fetchMedicalList = async () => {
+  const { data } = await axios.get(URL);
+  const placeInfos: [] = data.data;
+  const medicalList = placeInfos.filter((placeInfo: any) => placeInfo['카테고리2'] == '반려의료'); // 전체 10개면 7개/ 100개면 88개는 반려의료
+
+  // console.log(medicalList.length);
+  return medicalList;
 };
--- app/medical/page.tsx
+++ app/medical/page.tsx
@@ -1,12 +1,74 @@
-import React from 'react';
+'use client';
 
-// ssr 은 아니고 isr 로 하기 .. 근데 다른api쓰지않는한 안바뀜? 22년11.30기준데이터.  ssg로 하고 url바뀌면 빌드다시하면되는거아닌가 ㅎ.
-// 근데 일단 캐싱될수있어서 주의?
+import React, { useEffect, useState } from 'react';
+import { fetchMedicalList } from '../_api/placeInfo-api';
 
 const Medical = () => {
+  const [medicalList, setMedicalList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const medicalList = await fetchMedicalList();
+      setMedicalList(medicalList);
+    };
+    fetchData();
+  }, []);
+
+  const [page, setPage] = useState<number>(1); // 현재 페이지 수
+  const totalNum = medicalList.length; // 총 게시물 수
+  const pageRange = 2; // 페이지당 보여줄 게시물 수 -   바꾸기
+  const btnRange = 7; // 보여질 페이지 버튼의 개수
+  const currentSet = Math.ceil(page / btnRange); // 현재 페이지 세트
+  const startPage = (currentSet - 1) * btnRange + 1; // 시작 페이지 번호
+  const endPage = Math.min(startPage + btnRange - 1, Math.ceil(totalNum / pageRange)); // 마지막 페이지 번호
+
+  if (!medicalList) {
+    <div>데이터를 가져오지 못했습니다. 다시 시도해주세요.</div>;
+  }
+
   return (
-    <section className="h-screen flex items-center justify-center">
-      <div className="text-5xl">공사중입니다!</div>
+    <section className="flex items-center justify-center">
+      <div className="flex flex-col items-center bg-primaryColor/10 w-[1280px] min-h-[500px] ">
+        <nav className="flex justify-center w-[1200px] mt-16 mb-5 rounded-lg p-2">
+          <ul className="flex gap-10 items-center justify-center px-1">
+            <li>전체</li>
+            <li>서울</li>
+          </ul>
+        </nav>
+        <hr className="bg-gray-300/70 w-[1150px]" />
+        <section className="m-20 w-full flex flex-col justify-center items-center gap-5">
+          {medicalList?.map((medicalItem, idx) => {
+            return (
+              <div
+                key={idx}
+                className="flex flex-col gap-2 justify-center items-start border-[1px] border-gray-300 rounded-lg p-5 w-10/12"
+              >
+                <p>{medicalItem['카테고리3']}</p>
+                <p>시설명 : {medicalItem['시설명']}</p>
+                <p>전화번호 : {medicalItem['전화번호']}</p>
+                <p>운영시간 : {medicalItem['운영시간']}</p>
+                <p>주소 : {medicalItem['도로명주소']}</p>
+                <p>휴무일 : {medicalItem['휴무일']}</p>
+                <p>홈페이지 : {medicalItem['홈페이지']}</p>
+              </div>
+            );
+          })}
+        </section>
+        <section className="flex justify-between mt-[30px] px-20 w-full">
+          <nav className="flex gap-5">
+            {Array(btnRange)
+              .fill(startPage)
+              .map((_, i) => {
+                return (
+                  <button key={i} onClick={() => setPage(startPage + i)}>
+                    {/* // $active={page === startPage + i} */}
+                    {startPage + i}
+                  </button>
+                );
+              })}
+          </nav>
+        </section>
+      </div>
     </section>
   );
 };

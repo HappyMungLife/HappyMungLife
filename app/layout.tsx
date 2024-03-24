85e91a95e680c9d0c0b144d42e5f1740c3cdfa91
박서영 <seokachuu@gmail.com>
2024-03-25 01:03:57 +0900

    Fix: main html 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <Header />
         <Nav />
-        <main>
-          <QueryProvider>{children}</QueryProvider>
-        </main>
+        <QueryProvider>{children}</QueryProvider>
         <Footer />
       </body>
     </html>
